e848ea42da1ac4c448d254147299b45fa7d23225
evgbabenko <evgbabenko@live.com>
2022-07-18 14:52:02 +0300

    react

--- lesson-3/src/components/main/index.jsx
+++ lesson-3/src/components/main/index.jsx
@@ -8,6 +8,7 @@ import Section from '../section';
 
 const currency = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json';
 const banks = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/basindbank?date=20160101&period=m&json';
+const agr = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/monetary?sort=ind&order=asc&date=yyyymmdd&json';
 
 
 export default class Main extends Component {
@@ -16,13 +17,14 @@ export default class Main extends Component {
       <main>
         
           <header>
-            <Link to='/'><Btn btnName='Currency' /></Link>
-            <Link to='/bank'><Btn btnName='Bank info' /></Link>
-            <Btn btnName='Djlrf' />
+            <Link to='/'><Btn btnName='Офіційний курс гривні до іноземних валют та облікова ціна банківських металів' /></Link>
+            <Link to='/bank'><Btn btnName='Основні показники діяльності банків України' /></Link>
+            <Link to='/agr'><Btn btnName='Грошові агрегати' /></Link>
         </header>
         <Routes>
           <Route path="/" element={<Section url={currency} />} />
           <Route path="/bank" element={<Section url={banks} />} />
+          <Route path="/agr" element={<Section url={agr} />} />
         </Routes>
       </main>
     );
--- lesson-3/src/components/section/index.jsx
+++ lesson-3/src/components/section/index.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { requestData } from '../../request';
 import CircularProgress from '@mui/material/CircularProgress';
-import Box from '@mui/material/Box';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -9,7 +8,6 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-
 
 export default class Section extends Component {
     state = {
@@ -36,18 +34,18 @@ export default class Section extends Component {
     render() {
         const { allCurrency } = this.state;
         return (
-            <TableContainer component={Paper}>
-                <Table sx={{ maxWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Назва валюти</TableCell>
-                            <TableCell align="right">Курс</TableCell>
-                            <TableCell align="right">Код</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {Array.isArray(allCurrency) ?
-                            allCurrency.map((e, i) => {
+            Array.isArray(allCurrency) ?
+                (<TableContainer component={Paper}>
+                    <Table sx={{ maxWidth: 650 }} aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Назва валюти</TableCell>
+                                <TableCell align="right">Курс</TableCell>
+                                <TableCell align="right">Код</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {allCurrency.map((e, i) => {
                                 return (
                                     <TableRow key={i * 21 + 'cc'} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
                                         <TableCell component="th" scope="row">{e.txt}</TableCell>
@@ -55,10 +53,11 @@ export default class Section extends Component {
                                         <TableCell align="right">{e.cc}</TableCell>
                                     </TableRow>
                                 )
-                            }) : (<Box sx={{ display: 'flex' }}><CircularProgress /></Box>)}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+                            })}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                ) : (<CircularProgress />)
         )
     }
 }
